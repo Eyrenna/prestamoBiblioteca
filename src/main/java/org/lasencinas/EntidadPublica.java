44ac0fec7fdd98fcf8de2b140b548bf0bb66eb50
Claudia Gázquez Alessio <claudia365x@gmail.com>
2019-03-18 19:11:25 +0100

    Clase EntidadPublica con: atributos, constructor, seters y geters

--- src/main/java/org/lasencinas/EntidadPublica.java
+++ src/main/java/org/lasencinas/EntidadPublica.java
@@ -2,7 +2,38 @@ package org.lasencinas;
 
 public class EntidadPublica {
     //Atributos
+
     private String code = null;
     private String nombre = null;
     private String direccion = null;
+
+    //Constructor
+
+    public EntidadPublica(String code) {
+        this.code = code;
+    }
+
+    //Seters
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    //Geters
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
 }
